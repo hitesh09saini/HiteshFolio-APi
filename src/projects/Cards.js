8f60838e9ecaeb247ea928a502528112fb65e7b7
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-04 23:27:44 +0530

    edit code

--- src/projects/Cards.js
+++ src/projects/Cards.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Cards = ({ img, title, description, Vlink, link }) => {
   return (
     <div className="project-card">
-      <img 
-      src={img}
-      alt="new"
+      <img
+        src={img}
+        alt="new"
       />
       <h2>{title}</h2>
       <p>{description}</p>
       <div className='cards-btn'>
-        <a href={Vlink}>Visit</a>
-        <a href={link}>Code</a>
+        <a href={Vlink} target="_blank" rel="noreferrer">Visit</a>
+        <a href={link} target="_blank" rel="noreferrer">Code</a>
       </div>
     </div>
   );
