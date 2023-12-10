81d4573908baebb94605bbb65690f5dd3c17db79
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-11 02:00:48 +0530

    push

--- src/Contacts/ContactForm.js
+++ src/Contacts/ContactForm.js
@@ -1,38 +1,47 @@
-
 import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-
 
 function ContactForm() {
   const form = useRef();
 
-  const sendEmail = (e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_3yhomxg', 'template_55m2o7a', form.current, 'CnH-TtgGn1JGZaTBC')
-      .then((result) => {
-        console.log(result.text);
-        console.log("Message Sent");
-      }, (error) => {
-        console.log(error.text);
+
+    const formData = new FormData(form.current);
+    const data = {
+      email: formData.get('user_email'),
+      name: formData.get('user_name'),
+      message: formData.get('message'),
+    };
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/send/mail`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
       });
+
+      if (response.ok) {
+        console.log('Mail sent successfully');
+        // You may want to reset the form or show a success message here
+      } else {
+        console.error('Failed to send mail');
+        // Handle the error, show an error message, etc.
+      }
+    } catch (error) {
+      console.error('Error sending mail:', error);
+    }
   };
 
-
-
   return (
-
     <form ref={form} onSubmit={sendEmail}>
-
-      <input type="text" name="user_name" placeholder='Name' />
-
-      <input type="email" name="user_email" placeholder='Your email' />
+      <input type="text" name="user_name" placeholder="Name" />
+      <input type="email" name="user_email" placeholder="Your email" />
       <label htmlFor="message">Message:</label>
-      <textarea name="message" id='message' placeholder='Hye !' />
-      <input type="submit" id='submit' value="Send" />
-
+      <textarea name="message" id="message" placeholder="Hey!" />
+      <input type="submit" id="submit" value="Send" />
     </form>
-
-
   );
 }
 
